2bb175697dc7be5af9f8a68d5c8af82f003d60ed
rodrdan <daniel.dan.rodr@gmail.com>
2024-05-10 22:09:16 +0200

    initial commit

---
+++ microservices-cards/cards/src/main/java/cz/rodr/cards/dto/CardContactInfoDto.java
@@ -0,0 +1,18 @@
+package cz.rodr.cards.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.List;
+import java.util.Map;
+
+@ConfigurationProperties(prefix = "cards")
+@Getter
+@Setter
+public class CardContactInfoDto {
+
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
+}
